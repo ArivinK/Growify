e3200dc1a01f7524beafbb4b6292ef0456d0f3eb
Victor Verhaegen <Victor@Victors-MacBook-Pro.local>
2016-10-19 15:37:28 +0200

    Worked on these 2 files

--- controllers/products.js
+++ controllers/products.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const Product = require("../models/connection");
+const product = require("../models/connection");
 const ObjectID = require('mongodb').ObjectID;
 
 // router.get('/', (req,res) => {
-//     Product.all(function(result){
+//     product.all(function(result){
 //             res.redirect('/products')
 //         }
 //     )
 // })
-Product.connect();
+
+product.connect((err, result) => {
+    if (err) {console.log (err)};
+    console.log("Connected");
+});
+
 
 router.get('/', (req, res) => {
     res.redirect('/products')
 })
 
 router.get('/products/', (req, res) => {
-    Product.getAll("stock", function(result) {
+    product.getAll("stock", function(result) {
         res.render('products.ejs', {
             products: result
         })
@@ -24,7 +29,7 @@ router.get('/products/', (req, res) => {
 })
 
 router.get('/buy/:id', (req, res) => {
-    Product.get("stock", ObjectID(req.params.id), function(result) {
+    product.get("stock", ObjectID(req.params.id), function(result) {
         res.render('buy.ejs', {
             products: result
         })
@@ -32,13 +37,13 @@ router.get('/buy/:id', (req, res) => {
 })
 
 router.get('/buy/confirmation/:id', (req, res) => {
-    Product.buy(ObjectID(req.params.id), function(result) {
+    product.buy(ObjectID(req.params.id), function(result) {
         res.render('confirmation.ejs', {products: result})
     })
 })
 
 // router.delete('/buy/confirmation/:id', (req, res) => {
-//     Product.findOneAndDelete({
+//     product.findOneAndDelete({
 //         "_id": id
 //     }, (err, result) => {
 //         if (err) return res.send(err)
